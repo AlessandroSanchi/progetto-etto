2742e9336c26cd76160e89eab41ae8e1d05f1a82
Alessandro <alessandro.sanchi52@studenti.isissgobetti.it>
2024-04-04 13:47:40 +0200

    iniziato

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+import os
+import art
+money = 0
+def inputpage():
+ print("-1 Farm")
+ print("-2 Shop")
+ print("-3 Fight")
+ print("-4 Quit")
+
+oggetti = ["sassi"]
+>>> tprint("E")
+print(f"money:{money}")
+inputpage()
+
+input = input("che si fa?")
+
+
+
+match input:
+    case "1":
+        print("Farming...")#"banning random people", "getting these money"])
+
+    case "2":
+      print("Welcome to the Shop!")
+
+
+
+    case "3": 
+      print("Fighting")
+
+    case "4":
+      print("Leaving...")
+    
+
